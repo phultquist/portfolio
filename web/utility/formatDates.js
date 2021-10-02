e868480dd5cb80b3102a7731f10435695d1e6963
Rustom Ichhaporia <45826892+rustom@users.noreply.github.com>
2021-10-02 02:44:19 -0500

    You spelled anticipated wrong

--- web/utility/formatDates.js
+++ web/utility/formatDates.js
@@ -18,7 +18,7 @@ export default function formatDates(a, b) {
 
     if (b) {
         let b_date = new Date(b);
-        endDateText = formatDate(b_date) + (b_date > new Date() ? " (ancitipated)" : "");
+        endDateText = formatDate(b_date) + (b_date > new Date() ? " (anticipated)" : "");
     } else {
         endDateText = "Present";
     }
