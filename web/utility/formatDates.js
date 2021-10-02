96121c7d7eddecb13dce47394caae722f5c98d64
Rustom Ichhaporia <45826892+rustom@users.noreply.github.com>
2021-10-02 02:25:14 -0500

    Undo prettier's autoformatting

--- web/utility/formatDates.js
+++ web/utility/formatDates.js
@@ -1,50 +1,49 @@
 export default function formatDates(a, b) {
-  let startDateText = "";
-  let endDateText = "";
+    let startDateText = "";
+    let endDateText = "";
 
-  if (b && !a) {
-    [a, b] = [b, a];
-  } else if (!a && !b) {
-    throw new Error("Neither date is defined.");
-  } else if (a > b) {
-    [b, a] = [a, b];
-  }
+    if (b && !a) {
+        [a, b] = [b, a];
+    } else if (!a && !b) {
+        throw new Error("Neither date is defined.");
+    } else if (a > b) {
+        [b, a] = [a, b];
+    }
 
-  if (a) {
-    startDateText = formatDate(a);
-  } else {
-    startDateText = "Ambiguous beginnings";
-  }
+    if (a) {
+        startDateText = formatDate(a);
+    } else {
+        startDateText = "Ambiguous beginnings";
+    }
 
-  if (b) {
-    let b_date = new Date(b);
-    endDateText =
-      formatDate(b_date) + (b_date > new Date() ? " (ancitipated)" : "");
-  } else {
-    endDateText = "Present";
-  }
+    if (b) {
+        let b_date = new Date(b);
+        endDateText = formatDate(b_date) + (b_date > new Date() ? " (ancitipated)" : "");
+    } else {
+        endDateText = "Present";
+    }
 
-  return startDateText + " - " + endDateText;
+    return startDateText + " - " + endDateText;
 }
 
 function formatDate(date) {
-  if (!(date instanceof Date)) {
-    date = new Date(date);
-  }
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+    if (!(date instanceof Date)) {
+        date = new Date(date);
+    }
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
 
-  return months[date.getMonth()] + " " + date.getFullYear();
+    return months[date.getMonth()] + " " + date.getFullYear();
 }
