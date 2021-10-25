f8e38a7c2e27bc9929a5bd424e154a99db7b0131
Patrick Hultquist <patrick@patrick.today>
2021-10-25 00:54:42 -0400

    grid cols

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    safeList: [
+      'grid-cols-1',
+      'grid-cols-2',
+      'grid-cols-3',
+      'grid-cols-4',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
