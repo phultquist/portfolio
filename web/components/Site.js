6985a71ac8780546ac7e0aeab23730b68707f85a
Patrick Hultquist <patrick@patrick.today>
2021-11-27 23:32:09 -0600

    small ui changes

--- web/components/Site.js
+++ web/components/Site.js
@@ -4,14 +4,13 @@ export default function Component({ url }) {
         <div className="site w-full shadow rounded-t-md overflow-hidden bg-white">
             <div className="border-b" style={{ backgroundColor: "#fefefe" }}>
                 <div className="absolute flex flex-row space-x-2 py-4 px-6">
-                    {/* add the menu icons */}
                     {["bg-red-400", "bg-yellow-400", "bg-green-400"].map(color => {
                         return <div className={`rounded-full ${color} w-3 h-3 ring-1 ring-gray-200 ring-opacity-50`}></div>
                     })}
                 </div>
                 <div className="w-full flex justify-center">
                     <div className="w-3/5 flex justify-center my-2 py-1 rounded text-sm" style={{ backgroundColor: "#f1f1f1" }}>
-                        <a href={url}>{hostname}</a>
+                        <a href={url} target="_blank">{hostname}</a>
                     </div>
                 </div>
             </div>
