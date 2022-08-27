c083b27ace5d25f69564ebff7d22666ec5240f0c
Patrick Hultquist <patrick@monogram.io>
2022-08-27 13:32:44 -0400

    move og-image

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -8,10 +8,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
         <title>Patrick Hultquist</title>
         <meta name="robots" content="index, follow" />
-        <meta
-          property="og:image"
-          content="https://patrick.today/og-image.jpg"
-        />
+        <meta property="og:image" content="https://patrick.app/og-image.jpg" />
         <script type="text/javascript">
           {`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
         heap.load("4089219467");`}
