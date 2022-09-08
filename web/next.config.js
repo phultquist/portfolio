7e57e33bc0e531b7445808c528cb90797910a325
Patrick Hultquist <patrick@monogram.io>
2022-09-09 01:44:47 +0200

    Update next.config.js

--- web/next.config.js
+++ web/next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
+  redirects() {
+    return [{
+      "source": "/autostore",
+      "destination: "https://gist.github.com/phultquist/e9cb115417398e5cb75e75cfed2b9c4b"
+    }]
+  }
 }
